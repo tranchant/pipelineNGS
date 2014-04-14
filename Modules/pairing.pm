08bafa1328f84e4ba0ed6b6c1359376bef3dd311
cmonat <cecile.monat@ird.fr>
2014-04-14 09:27:43 +0200

    licence

--- Modules/pairing.pm
+++ Modules/pairing.pm
@@ -3,7 +3,7 @@
 # Licencied under CeCill-C (http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html) and GPLv3
 #
 # Intellectual property belongs to IRD, CIRAD and SouthGreen developpement plateform 
-# Written by Cécile Monat, Ayite Kougbeadjo, Mawusse Agbessi, Christine Tranchant, Marilyne Summo, Cédric Farcy, François Sabot
+# Written by Cécile Monat, Ayité Kougbeadjo, Mawusse Agbessi, Christine Tranchant, Marilyne Summo, Cédric Farcy, François Sabot
 #
 ###################################################################################################################################
 
