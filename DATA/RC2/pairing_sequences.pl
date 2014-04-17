f61d5a61f956ffb113ff86c0b05b489f8876384d
cmonat <cecile.monat@ird.fr>
2014-04-17 14:13:26 +0200

    modif chdir en cd pour test RC2

--- DATA/RC2/pairing_sequences.pl
+++ DATA/RC2/pairing_sequences.pl
@@ -6,7 +6,7 @@ use warnings;
 use lib qw (teams/ggr/pipelineNGS/Modules/);
 use pairing;
 
-chdir 'teams/ggr/pipelineNGS/DATA/RC2/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
+cd 'teams/ggr/pipelineNGS/DATA/RC2/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
 my $reverse='teams/ggr/pipelineNGS/DATA/RC2/2_CUTADAPT/RC2_1.CUTADAPT.fastq';
 my $forward='teams/ggr/pipelineNGS/DATA/RC2/2_CUTADAPT/RC2_2.CUTADAPT.fastq';
 pairing::repairing($reverse,$forward);
