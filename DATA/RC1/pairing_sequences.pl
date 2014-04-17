b5ec3d60f60b03d46e6b6331c6c670855bfab6fc
cmonat <cecile.monat@ird.fr>
2014-04-17 14:09:59 +0200

    modification chemin pour pairing_sequence RC1

--- DATA/RC1/pairing_sequences.pl
+++ DATA/RC1/pairing_sequences.pl
@@ -6,7 +6,7 @@ use warnings;
 use lib qw (teams/ggr/pipelineNGS/Modules/);
 use pairing;
 
-chdir '~/DATA/RC1/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
-my $reverse='~/DATA/RC1/2_CUTADAPT/RC1_1.CUTADAPT.fastq';
-my $forward='~/DATA/RC1/2_CUTADAPT/RC1_2.CUTADAPT.fastq';
+chdir 'teams/ggr/pipelineNGS/DATA/RC1/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
+my $reverse='teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_1.CUTADAPT.fastq';
+my $forward='teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_2.CUTADAPT.fastq';
 pairing::repairing($reverse,$forward);
