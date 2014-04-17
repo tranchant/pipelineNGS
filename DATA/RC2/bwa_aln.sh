3b92cfeaac79bd0804a8ccb2e9b8a5c5441df426
cmonat <cecile.monat@ird.fr>
2014-04-17 15:01:24 +0200

    modification bwa_aln pour RC2

--- DATA/RC2/bwa_aln.sh
+++ DATA/RC2/bwa_aln.sh
@@ -1,2 +1,2 @@
-bwa aln Reference.fasta 3_PAIRING_SEQUENCES/RC2_1_CUTADAPT_forwardRepaired.fastq > 4_BWA/RC2_1.BWAALN.sai 
-bwa aln Reference.fasta 3_PAIRING_SEQUENCES/RC2_2_CUTADAPT_reverseRepaired.fastq > 4_BWA/RC2_2.BWAALN.sai
+bwa aln Reference.fasta 3_PAIRING_SEQUENCES/RC2_1_CUTADAPT.REPAIRING.fastq > 4_BWA/RC2_1.BWAALN.sai 
+bwa aln Reference.fasta 3_PAIRING_SEQUENCES/RC2_2_CUTADAPT.REPAIRING.fastq > 4_BWA/RC2_2.BWAALN.sai
