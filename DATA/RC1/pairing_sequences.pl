cbe6d2077bf59f6cd518a787b386c3eb726f2c78
cmonat <cecile.monat@ird.fr>
2014-04-17 14:31:14 +0200

    modification chemin erreur corrigée

--- DATA/RC1/pairing_sequences.pl
+++ DATA/RC1/pairing_sequences.pl
@@ -3,10 +3,10 @@
 use strict;
 use warnings;
 
-use lib qw (teams/ggr/pipelineNGS/Modules/);
+use lib qw (/teams/ggr/pipelineNGS/Modules/);
 use pairing;
 
-chdir 'teams/ggr/pipelineNGS/DATA/RC1/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
-my $reverse='teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_1.CUTADAPT.fastq';
-my $forward='teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_2.CUTADAPT.fastq';
+chdir '/teams/ggr/pipelineNGS/DATA/RC1/3_PAIRING_SEQUENCES' or die "Impossible to chdir";
+my $reverse='/teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_1.CUTADAPT.fastq';
+my $forward='/teams/ggr/pipelineNGS/DATA/RC1/2_CUTADAPT/RC1_2.CUTADAPT.fastq';
 pairing::repairing($reverse,$forward);
